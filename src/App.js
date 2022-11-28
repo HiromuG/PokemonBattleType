5a59973245cde423caba1380aab60d5043e1fb41
HiromuG <asas34520@gmail.com>
2022-11-28 12:08:37 +0800

    add twttr

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './App.css'
 import AppBtnArea from './AppBtnArea';
+import { Timeline } from 'react-twitter-widgets';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(()=>{
+    if(window.twttr){
+      window.twttr.widgets.load()
+    }
+  },[])
   return (
     <div className="App">
       <div className='App-title'>ポケモン<br />防御側タイプ相性表</div>
+      <div className='twittertimeline'>
+        Tweets from @Pokemon_cojp
+        <Timeline
+          dataSource={{
+            sourceType: 'profile',
+            screenName: 'Pokemon_cojp'
+          }}
+          options={{
+            id: 'profile:Pokemon_cojp',
+            tweetLimit: '8',
+          }}
+        />
+      </div>
       <AppBtnArea />
     </div>
   );
